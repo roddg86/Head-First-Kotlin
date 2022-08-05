77c557c945bb0f3b0aec333a027d80588f609310
roddg86 <roddg@mail.ru>
2022-08-06 07:34:37 +0900

    A

---
+++ MyFirstApp/src/main/kotlin/ Exercises.kt
@@ -0,0 +1,9 @@
+fun main (){
+    val hobbits = arrayOf("Frodo", "Sam", "Merry", "Pippin")
+    var x = 0;
+
+    while (x < 4){
+        println("${hobbits[x]} хорошее хобитское имя")
+        x = x + 1
+    }
+}
