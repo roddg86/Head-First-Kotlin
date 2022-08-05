4c97cd507305304e5a2d382ae7bc64d208a075b7
roddg86 <roddg@mail.ru>
2022-08-05 17:19:03 +0900

    A

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,10 +1,10 @@
 fun main() {
     var x = 1
 
-    while (x < 3) {
-        print(if (x == 1) "Yab" else "Dab")
-        print("ba")
+    while (x < 10) {
         x = x + 1
+        if (x > 3) {
+            println("big x")
+        }
     }
-    if (x == 3) println("Do")
 }
