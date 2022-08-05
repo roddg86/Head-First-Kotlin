6cfd2aa76e898dd219cc66ecbb76c48a1d7648d0
roddg86 <roddg@mail.ru>
2022-08-05 17:22:55 +0900

    B

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,10 +1,8 @@
 fun main() {
-    var x = 1
+    var x = 10 // val неизменяемая переменная
 
-    while (x < 10) {
-        x = x + 1
-        if (x > 3) {
-            println("big x")
-        }
+    while (x > 1) {
+        x = x - 1
+        if (x < 3) println("small x")
     }
 }
