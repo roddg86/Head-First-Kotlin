1b27086af924a5f10dd1b2489e5d8157967ab66b
roddg86 <roddg@mail.ru>
2022-08-06 07:58:36 +0900

    Link confusion

--- MyFirstApp/src/main/kotlin/ Exercises.kt
+++ MyFirstApp/src/main/kotlin/ Exercises.kt
@@ -1,17 +1,21 @@
 fun main() {
-    /* Fruit = Banana
-    *  Fruit = Blueberry
-    *  Fruit = Pomegranate
-    *  Fruit = Cherry */
+    val x = arrayOf(0,1,2,3,4)
+    x[3] = x[2]
+    x[4] = x[0]
+    x[2] = x[1]
+    x[1] = x[0]
+    x[0] = x[1]
+    x[4] = x[3]
+    x[3] = x[2]
+    x[2] = x[4]
 
-    val fruit = arrayOf("Apple", "Banana", "Cherry", "Blueberry", "Pomegranate")
+    println(x[3])
 
-    var x = 0
-    while (x < 4) {
-        val index = arrayOf(1, 3, 4, 2)
-        var y: Int
-        y = index[x]
-        println("Fruit = ${fruit[y]}")
-        x = x + 1
-    }
+    /*
+    * x[0] = 0
+    * x[1] = 0
+    * x[2] = 2
+    * x[3] = 1
+    * x[4] = 2
+    * */
 }
