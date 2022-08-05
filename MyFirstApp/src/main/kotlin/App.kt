3172f46df298201282a42490030a25f83359f22b
roddg86 <roddg@mail.ru>
2022-08-05 17:48:36 +0900

    code blocks

--- MyFirstApp/src/main/kotlin/App.kt
+++ MyFirstApp/src/main/kotlin/App.kt
@@ -1,8 +1,26 @@
 fun main() {
-    var x = 10
+    var x = 0
+    var y = 0
 
-    while (x > 1) {
-        x = x - 1
-        print(if (x < 3) "small x" else "big x")
+    while (x < 5) {
+        //y = x - y // 00 11 21 32 42
+
+        //y = y + x // 00 11 23 36 410
+
+        /*y = y + 3
+        if (y > 4) y = y - 1 // 03 15 27 39 411
+        */
+
+        /*x = x + 2
+        y = y + x // 22 57*/
+
+        /*if (y < 5){
+            x = x + 1
+            if (y < 3) x = x - 1
+        }
+        y = y + 3 // 03 26 39 412 */
+
+        print("$x$y ")
+        x = x + 1
     }
 }
