a67a7d51924dc0bfd6217d059b2c240e52e25f89
roddg86 <roddg@mail.ru>
2022-08-06 09:52:54 +0900

    С

--- RockPaperScissors/src/main/kotlin/ExercisesFun.kt
+++ RockPaperScissors/src/main/kotlin/ExercisesFun.kt
@@ -1,9 +1,17 @@
 fun main() {
-    println(timesThree(4))
+    val mass = arrayOf(1,2,3,4,5)
+
+    println(maxValue(mass))
 }
 
-fun timesThree(x: Int): Int {
-    var y = 0
-    y = x * 3
-    return y
+/* Некомпелировался из за того что неуказан тип возвращаемого значения в функции */
+fun maxValue(args: Array<Int>): Int {
+    var max = args[0]
+    var x = 1
+    while (x < args.size){
+        var item = args[x]
+        max = if (max >= item) max else item
+        x = x + 1
+    }
+    return max
 }
