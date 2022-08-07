ba31f53c4eb88c5f641fcdf4fef4ad358a972720
roddg86 <roddg@mail.ru>
2022-08-07 13:45:03 +0900

    Songs

--- Songs/src/main/kotlin/Songs.kt
+++ Songs/src/main/kotlin/Songs.kt
@@ -1,2 +1,10 @@
-class Songs {
+/* Определяем свойства title и artist */
+class Song (val title: String, val artist: String) {
+    fun play(){
+        println("Воспроизведение песни $title в исполнении $artist")
+    }
+
+    fun stop(){
+        println("Прекращено воспроизведение $title")
+    }
 }
