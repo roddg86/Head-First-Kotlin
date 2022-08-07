b3e88b99a2ef746e3550a1da8791e72bb2458842
roddg86 <roddg@mail.ru>
2022-08-07 14:21:46 +0900

    Constructors

--- Songs/src/main/kotlin/Main.kt
+++ Songs/src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 fun main(args: Array<String>) {
 
-    /* Создаем три обьекта Song */
-    val songOne = Song("Месопотамцы", "Они могут быть великанами")
-    val songTwo = Song("Уходя в подполье", "Джем")
-    val songThree = Song("Заставь меня улыбнуться", "Стив Харли")
+    /* ding ding ba-da-bing!
+    bang bang bang!
+    ding ding ba-da-bing! */
 
-    songOne.play()
-    songTwo.stop()
-    songThree.play()
+    /* Создаем экземпляр класса */
+    val d = DrumKit(true, true)
 
+    /* Компилятор создал за нас пустой конструктор для класса Duck
+    * Вызываем пустой конструктор для создания экземпляра Duck
+    * */
+    var myDuck = Duck()
+
+    d.playTopHat()
+    d.playSnare()
+
+    d.hasSnare = false
+
+    d.playTopHat()
+    d.playSnare()
 }
