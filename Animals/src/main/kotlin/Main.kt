35f13f626e780c71a9d36e28bdd13c01e899a8c4
roddg86 <roddg@mail.ru>
2022-08-07 20:13:09 +0900

    Polymorphism

--- Animals/src/main/kotlin/Main.kt
+++ Animals/src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
 fun main(args: Array<String>) {
-    val m = arrayOf(
-        Vampire(),
-        Dragon(),
-        Monster()
-    )
-    for (item in m) {
-        item.frighten()
+    /* Полиморфизм означает «много форм». Этот
+    механизм позволяет разным подклассам
+    переопределять разные реализации одной функции. */
+
+    val animals = arrayOf(Hippo(), Wolf())
+    /* В цикле перебирается массив с элементами Animal */
+    for (item in animals) {
+        item.roam()
+        item.eat()
     }
 
+    val vet = Vet()
+    val wolf = Wolf()
+    val hippo = Hippo()
+
+    /* Вызывается функция giveShot
+   * класса Vet, которой передается два подкласса Animal */
+    vet.giveShot(wolf)
+    vet.giveShot(hippo)
 }
