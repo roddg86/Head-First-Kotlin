9778b47f869e5242e310571f181198c5ca44a20e
roddg86 <roddg@mail.ru>
2022-08-08 15:09:01 +0900

    Null A

---
+++ Recipes/src/main/kotlin/Cat.kt
@@ -0,0 +1,5 @@
+class Cat(var name: String? = "") {
+    fun Meow() {
+        println("Meow!")
+    }
+}
--- Recipes/src/main/kotlin/Main.kt
+++ Recipes/src/main/kotlin/Main.kt
@@ -1,12 +1,11 @@
 fun main(args: Array<String>) {
-    val s1 = Student("Ron", "Weasley", "Gryffindor")
-    val s2 = Student("Draco", "Malfoy", house = "Slytherin")
-    val s3 = s1.copy(firstName = "Fred", year = 3)
-    val  s4 = s3.copy(firstName = "George")
-
-    val array = arrayOf(s1,s2,s3,s4)
-    /* Эта строка деструктуризует каждый обьект Student в массиве */
-    for ((firstName, lastName, house, year) in array){
-        println("$firstName $lastName is in $house year $year")
+    var myCats = arrayOf(Cat("Misty"),
+    null,
+    Cat("Socks"))
+    for (cat in myCats){
+        if (cat != null){
+            print("${cat.name}:")
+            cat.Meow()
+        }
     }
 }
