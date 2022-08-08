0e92b845c9a5f71714dd4abbdf72407e409a6d62
roddg86 <roddg@mail.ru>
2022-08-08 12:32:00 +0900

    default settings

--- Recipes/src/main/kotlin/Main.kt
+++ Recipes/src/main/kotlin/Main.kt
@@ -1,22 +1,42 @@
 fun main(args: Array<String>) {
-    var m1 = Movie("Blaack Panther", "2018")
-    var m2 = Movie("Jurassic World", "2015")
-    var m3 = Movie("Jurassic World", "2015")
+    /* Изменим вызов первичного конструктора */
+    val r1 = Recipe("Тайский карри", "Chicken")
+    val r2 = Recipe(title = "Тайский карри", mainIngredient = "Chicken")
 
-    var s1 = Song("Love Cats", "The Cure")
-    var s2 = Song("Wild Horses", "The Rolling Stones")
-    var s3 = Song("Love Cats", "The Cure")
+    /* создание копии обьекта r1 и изменение его свойства title, по итогу получаем новый обьект */
+    val r3 = r1.copy(title = "Куринная бхуна")
 
-    println(m2 == m3) // true
-    println(s1 == s3) // false
+    println("r1 hash code: ${r1.hashCode()}")
+    println("r2 hash code: ${r2.hashCode()}")
+    println("r3 hash code: ${r3.hashCode()}")
 
-    var m4 = m1.copy()
-    println(m1 == m4) // true
+    println("r1 toString: ${r1.toString()}")
 
-    var m5 = m1.copy()
-    println(m1 === m5) // false
+    /* == проверяет эквивалентность объектов. */
+    /* Условие r1 == r2 истинно, потому что их объекты имеют одинаковые значения */
+    println("r1 == r2? ${r1 == r2}")
 
-    var m6 = m2
-    m2 = m3
-    println(m3 == m6) // true
+    /* === проверяет тождественность объектов.
+    * Оператор === всегда проверяет, ссылаются ли две переменные на один объект.*/
+    /* так как переменные содержат ссылки на разные объекты, условие r1 === r2 ложно. */
+    println("r1 === r2? ${r1 === r2}")
+
+    println("r1 == r3? ${r1 == r3}")
+
+    /* Разбираем новые свойства Recipe при деструктуризации r1 */
+    val (title, mainIngredient, vegetarian, difficulty) = r1
+    println("title is $title and vegetarian is $vegetarian")
+
+    /* Создание обьета Mushroom вызовом первичного конструктора */
+    val m1 = Mushroom(6, false)
+    println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
+
+    /* Создание обьекта Mushroom вызовом вторичного конструктора */
+    val m2 = Mushroom(true)
+    println("m2 size is ${m2.size} and isMagic is ${m2.isMagic}")
+
+    /* вызов int - версии addNumbers */
+    println(addNumbers(2, 5))
+    /* вызов Double - версии addNumbers */
+    println(addNumbers(1.6, 7.3))
 }
--- Recipes/src/main/kotlin/Recipes.kt
+++ Recipes/src/main/kotlin/Recipes.kt
@@ -1,8 +1,36 @@
-/* Классом данных называется класс для создания объектов, предназначенных для
-хранения данных.
+/* Первичный конструктор изменился */
+data class Recipe(
+    val title: String,
+    val mainIngredient: String,
+    /* заданы значения по умолчанию */
+    val isVegetarian: Boolean = false,
+    val difficulty: String = "Easy"
+) {}
 
-Для работы с данными классу данных необходимы объекты, поэтому он автома-
-тически предоставляет следующие реализации для функций equals, hashCode
-и toString, унаследованных от суперкласса Any
- */
-data class Recipe(val title: String, val isVegetarian: Boolean)
+/* Класс со вторичным конструктором */
+class Mushroom(val size: Int, val isMagic: Boolean) {
+    constructor(isMagic_param: Boolean) : this(0, isMagic_param)
+    //Код, выполняемый при вызове вторичного конструктора
+}
+
+/* Пример функции использующей параметры по умолчанию */
+fun findRecipes(
+    title: String = "",
+    ingredient: String = "",
+    isVegetarian: Boolean = false,
+    difficulty: String = ""
+): Array<Recipe> {
+
+    //Код поиска рецептов
+
+    return arrayOf(Recipe(title, ingredient, isVegetarian, difficulty))
+}
+
+/* Перегруженные функции */
+fun addNumbers(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun addNumbers(a: Double, b: Double): Double {
+    return a + b
+}
