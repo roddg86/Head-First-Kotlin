3d2fe8d8a580dff0da7725d5e584da40e0bf9d06
roddg86 <roddg@mail.ru>
2022-08-09 11:10:06 +0900

    Set MutableSet

--- Collections/src/main/kotlin/Collections.kt
+++ Collections/src/main/kotlin/Collections.kt
@@ -1,27 +1,45 @@
 fun main(){
 
-    /*
-    [Zero, Two, Four, Six]
-    [Two, Four, Six, Eight]
-    [Two, Four, Six, Eight, Ten]
-    [Two, Four, Six, Eight, Ten]
-    */
-    var a: MutableList<String> = mutableListOf()
-    a.add(0, "Zero")
-    a.add(1, "Two")
-    a.add(2, "Four")
-    a.add(3,"Six")
-    println(a)
-    if (a.contains("Zero")) a.add("Eight")
-    a.removeAt(0)
-    println(a)
-    if (a.indexOf("Four") != 4) a.add("Ten")
-    println(a)
-    if (a.contains("Zero")) a.add("Twelve")
-    println(a)
+    /* изменяеый список */
+    var mShoppingList = mutableListOf("Чай","Яйца","Молоко")
+    println("mShoppingList оригинальный: $mShoppingList")
 
+    /* неизменяемый список */
+    val extraShopping = listOf("Печенье", "Сахар", "Яйца")
 
+    /* добавим элементы неизменяемого списка в изменяемый */
+    mShoppingList.addAll(extraShopping)
+    println("mShoppingList элементы добавлены: $mShoppingList")
 
+    /* Если изменяемый список содержит Tea */
+    if (mShoppingList.contains("Чай")){
+        /*  set - обновить значение, значение с индексом Чай заменим на значение с индексом Кофе */
+        mShoppingList.set(mShoppingList.indexOf("Чай"), "Кофе")
+        println("mShoppingList значение заменяно: $mShoppingList")
+    }
+    /* Отсортируем список в естественном порядке (по алфавиту) */
+    mShoppingList.sort()
+    println("mShoppingList отсортированный: $mShoppingList")
 
+    /* Перевернутый список */
+    mShoppingList.reverse()
+    println("mShoppingList перевернутый: $mShoppingList")
 
+    /* скопируем список в новый обьект */
+    val mShoppingSet = mShoppingList.toMutableSet()
+
+    /* коллекция set удалила дубликаты */
+    println("mShoppingSet: $mShoppingSet")
+
+    /* создадим неизменяемую неупорядоченную коллекцию без повторяющихся значений*/
+    val moreShopping = setOf("Зеленый лук", "Шпинат", "Молоко")
+
+    /* добавим элементы неизменяемой коллекции в изменяемую */
+    mShoppingSet.addAll(moreShopping)
+
+    println("mShoppingSet элементы добавлены: $mShoppingSet")
+
+    /* скопируем коллекцию Set в список List */
+    mShoppingList = mShoppingSet.toMutableList()
+    println("mShoppingList новая врсия: $mShoppingList")
 }
