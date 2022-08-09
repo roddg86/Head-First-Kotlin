0ce51b7f7f59f784f4f38c88922bb234d1554678
roddg86 <roddg@mail.ru>
2022-08-09 09:58:28 +0900

    Exercises List

--- Collections/src/main/kotlin/Collections.kt
+++ Collections/src/main/kotlin/Collections.kt
@@ -1,26 +1,27 @@
 fun main(){
-    /* изменяеый список */
-    val mShoppingList = mutableListOf("Чай","Яйца","Молоко")
-    println("mShoppingList оригинальный: $mShoppingList")
 
-    /* неизменяемый список */
-    val extraShopping = listOf("Печенье", "Сахар", "Яйца")
+    /*
+    [Zero, Two, Four, Six]
+    [Two, Four, Six, Eight]
+    [Two, Four, Six, Eight, Ten]
+    [Two, Four, Six, Eight, Ten]
+    */
+    var a: MutableList<String> = mutableListOf()
+    a.add(0, "Zero")
+    a.add(1, "Two")
+    a.add(2, "Four")
+    a.add(3,"Six")
+    println(a)
+    if (a.contains("Zero")) a.add("Eight")
+    a.removeAt(0)
+    println(a)
+    if (a.indexOf("Four") != 4) a.add("Ten")
+    println(a)
+    if (a.contains("Zero")) a.add("Twelve")
+    println(a)
 
-    /* добавим элементы неизменяемого списка в изменяемый */
-    mShoppingList.addAll(extraShopping)
-    println("mShoppingList элементы добавлены: $mShoppingList")
 
-    /* Если изменяемый список содержит Tea */
-    if (mShoppingList.contains("Чай")){
-        /*  set - обновить значение, значение с индексом Чай заменим на значение с индексом Кофе */
-        mShoppingList.set(mShoppingList.indexOf("Чай"), "Кофе")
-        println("mShoppingList значение заменяно: $mShoppingList")
-    }
-    /* Отсортируем список в естественном порядке (по алфавиту) */
-    mShoppingList.sort()
-    println("mShoppingList отсортированный: $mShoppingList")
 
-    /* Перевернутый список */
-    mShoppingList.reverse()
-    println("mShoppingList перевернутый: $mShoppingList")
+
+
 }
