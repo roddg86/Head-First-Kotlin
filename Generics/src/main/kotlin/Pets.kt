e3bb56b355aebdd8c6d37dadaa0c4f4e6406e62d
roddg86 <roddg@mail.ru>
2022-08-11 14:33:18 +0900

    Creating the getWinners Function

--- Generics/src/main/kotlin/Pets.kt
+++ Generics/src/main/kotlin/Pets.kt
@@ -9,16 +9,20 @@ class Fish(name: String) : Pet(name)
 class Contest<T: Pet>{
 
     val scores: MutableMap<T, Int> = mutableMapOf()
-
-    /* Добавить участника и его оценку в MutableMap при условии, что оценка
-    больше либо равна 0.
-
-    Участник и оценка передаются функции в параметрах;
-    */
     fun addScore(t: T, score: Int = 0){
-        /* если оценка равна 0 или выше, функция добавляет данные в MutableMap как пару «ключ-значение». */
         if(score >= 0) scores.put(t, score)
     }
 
+    fun getWinners(): MutableSet<T>{
+        /* получить максимальное значение из score */
+        val highScore = scores.values.max()
+        val winners: MutableSet<T> = mutableSetOf()
+        for ((t, score) in scores){
+            /* Включить участников с максимальной оценкой в MutableSet. */
+            if (score == highScore) winners.add(t)
+        }
+        /* Вернуть множество MutableSet с данными победителей. */
+        return winners
+    }
 
 }
