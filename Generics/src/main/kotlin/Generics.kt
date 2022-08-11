a27966641b10f89b026ecc1450491a42f57317f4
roddg86 <roddg@mail.ru>
2022-08-11 13:54:20 +0900

    Generic type operations

--- Generics/src/main/kotlin/Generics.kt
+++ Generics/src/main/kotlin/Generics.kt
@@ -1,33 +1,17 @@
 class Generics {
-    /* Определим переменную для хранения ссылки на список с элеметами строк
-    * тип элемента в <> значит что это обобщение
-    *  */
-    /* означает, что «E» превращается в «String» в любой функции или
-    объявлении переменной, в которых используется «E» */
 
-    val x: MutableList<String> = TODO()
+    /* Создание экземпляра обобщенного класса. */
+    val duckList: MutableList<Duck>
+    duckList = mutableListOf(Duck("Donald"), Duck("Daisy"), Duck("Daffy"))
+    val list = mutableListOf("Fee", "Fi", "Fum")
+
+    /* Создание функции, получающей обобщенный тип. */
+    fun quack(ducks: MutableList<Duck>) {
+        //...
+    }
+
+    /* Создание функции, возвращающей обобщенный тип. */
+    fun getDucks(breed: String): MutableList<Duck> {
+        //Получение объектов Duck нужного вида
+    }
 }
-
-/*
-
-    Он интерпретируется компилятором в виде
-
-interface MutableList<String> : List<String>, MutableCollection<String> {
-    fun add(index: Int, element: String): Unit
-}
-
-*/
-
-/*
-
-    «E» — условное обозначение РЕАЛЬНОГО типа, который будет использоваться при объявлении MutableList.
-    MutableList наследуется от интерфейсов List и MutableCollection.
-    Тип, указанный вами для MutableList (значение «E»), автоматически используется в качестве типа List и MutableCollection.
-
-interface MutableList<E> : List<E>, MutableCollection<E>
-
-    Какой бы тип ни скрывался за «E», он определяет, какие объекты можно добавлять в MutableList.
-
-fun add(index: Int, element: E): Unit
-
- */
