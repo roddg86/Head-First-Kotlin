45fea90cf193ed9534440150ebf7a890a33facae
roddg86 <roddg@mail.ru>
2022-08-12 14:15:25 +0900

    A function can return a lambda

--- Lambdas/src/main/kotlin/Lambdas.kt
+++ Lambdas/src/main/kotlin/Lambdas.kt
@@ -1,17 +1,48 @@
-fun unless(condition: Boolean, code: () -> Unit) {
-    if (!condition) {
-        code()
-    }
-}
-
 fun main() {
-    val options = arrayOf("Red", "Amber", "Green")
-    var crossWalk = options[(Math.random() * options.size).toInt()]
-    if (crossWalk == "Green") {
-        println("Walk!")
-    }
-    unless(crossWalk == "Green") {
-        println("Stop!")
+
+    /* функция преобразования температуры по Цельсию в температуру по Фаренгейту */
+    fun convert(x: Double, converter: (Double) -> Double): Double {
+        val result = converter(x)
+        println("$x преобразуется в $result")
+        return result
     }
 
+    /* вызов функции аргументом которой будет лямбда выражение */
+    convert(20.0) { c: Double -> c * 1.8 + 32 }
+
+    /* краткая запись */
+    val fahrenheit = convert(36.6) { it * 1.8 + 32 }
+
+    fun convertFive(converter: (Int) -> Double): Double {
+        val result = converter(5)
+        println("5 преобразуется в $result")
+        return result
+    }
+
+    /* краткая запись, при вызове функции мы не используем круглые скобки потому что единственный параметр это лямбда */
+    convertFive {
+        it * 1.8 + 32
+    }
+    convert(22.0) { it * 1.8 + 32 }
+
+    /* функция возвращает одно лямбда выражение в зависимости от получаемой строки */
+    fun getConversionLambda(str: String): (Double) -> Double {
+        if (str == "CentigradeToFahrenheit") {
+            return { it * 1.8 + 32 }
+        } else if (str == "KgsToPounds") {
+            return { it * 2.204623 }
+        } else if (str == "PoundsToUSTons") {
+            return { it / 2000.0 }
+        } else {
+            return { it }
+        }
+    }
+
+    /* пересчет киллограммы в фунты */
+    val pounds = getConversionLambda("KgsToPounds")(2.5)
+    println(pounds)
+
+    /* функция getConversionLambda используется для получения лямбда-выражения, преобразующего
+     температуру из шкалы Цельсия в шкалу Фаренгейта, после чего передает ее функции convert: */
+    convert(20.0, getConversionLambda("CentigradeToFahrenheit"))
 }
