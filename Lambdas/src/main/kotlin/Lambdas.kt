1ca7871706793b55b268ce63ad1fe6d829a7f9a7
roddg86 <roddg@mail.ru>
2022-08-12 14:56:06 +0900

    type alias

--- Lambdas/src/main/kotlin/Lambdas.kt
+++ Lambdas/src/main/kotlin/Lambdas.kt
@@ -1,67 +1,48 @@
+/* добавим псевдоним типа, это означает что мы можем использовать DoubleConversion вместо (Double) -> Double */
+typealias DoubleConversion = (Double) -> Double
+
+/* функция преобразования температуры по Цельсию в температуру по Фаренгейту */
+fun convert(x: Double, converter: DoubleConversion): Double {
+    val result = converter(x)
+    println("$x преобразуется в $result")
+    return result
+}
+
+/* функция возвращает одно лямбда выражение в зависимости от получаемой строки */
+fun getConversionLambda(str: String): DoubleConversion {
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+/* определение параметров и возвращаемого типа, лямбда выражения */
+fun combine(
+    lambda1: DoubleConversion,
+    lambda2: DoubleConversion
+): DoubleConversion {
+    /* тело функции возвращает лямбда выражение */
+    return { x: Double -> lambda2(lambda1(x)) }
+}
+
 fun main() {
 
-    /* функция преобразования температуры по Цельсию в температуру по Фаренгейту */
-    fun convert(x: Double, converter: (Double) -> Double): Double {
-        val result = converter(x)
-        println("$x преобразуется в $result")
-        return result
-    }
-
-    /* вызов функции аргументом которой будет лямбда выражение */
-    convert(20.0) { c: Double -> c * 1.8 + 32 }
-
-    /* краткая запись */
-    val fahrenheit = convert(36.6) { it * 1.8 + 32 }
-
-    fun convertFive(converter: (Int) -> Double): Double {
-        val result = converter(5)
-        println("5 преобразуется в $result")
-        return result
-    }
-
-    /* краткая запись, при вызове функции мы не используем круглые скобки потому что единственный параметр это лямбда */
-    convertFive {
-        it * 1.8 + 32
-    }
-    convert(22.0) { it * 1.8 + 32 }
-
-    /* функция возвращает одно лямбда выражение в зависимости от получаемой строки */
-    fun getConversionLambda(str: String): (Double) -> Double {
-        if (str == "CentigradeToFahrenheit") {
-            return { it * 1.8 + 32 }
-        } else if (str == "KgsToPounds") {
-            return { it * 2.204623 }
-        } else if (str == "PoundsToUSTons") {
-            return { it / 2000.0 }
-        } else {
-            return { it }
-        }
-    }
-
-    /* пересчет киллограммы в фунты */
-    val pounds = getConversionLambda("KgsToPounds")(2.5)
-    println(pounds)
-
-    /* функция getConversionLambda используется для получения лямбда-выражения, преобразующего
-     температуру из шкалы Цельсия в шкалу Фаренгейта, после чего передает ее функции convert: */
-    convert(20.0, getConversionLambda("CentigradeToFahrenheit"))
-
-    /* определение параметров и возвращаемого типа, лямбда выражения */
-    fun combine(
-        lambda1: (Double) -> Double,
-        lambda2: (Double) -> Double
-    ): (Double) -> Double {
-        /* тело функции возвращает лямбда выражение */
-        return { x: Double -> lambda2(lambda1(x)) }
-    }
+    /* Преобразовать 2.5 кг в фунты */
+    println("Конвертировать 2.5кг в фунты: ${getConversionLambda("KgsToPounds")(2.5)}")
 
     /* создадим две переменные и присвоим им лямбда выражения */
-    val kgsToPounds = { x: Double -> x * 2.204623 }
-    val poundsToUSTons = { x: Double -> x / 2000.0 }
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
 
     /* передадим два лямбда выражения в функцию в качестве аргументов */
-    val kgsToUSTons = combine(kgsToPounds, poundsToUSTons)
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda,poundsToUSTonsLambda)
 
-    val usTons = kgsToUSTons(1000.0)
-    println(usTons)
+    val value = 17.4
+    println("$value кг это ${convert(value,kgsToUSTonsLambda)} тонн США")
+
 }
