dcff0bab8ac52b4b6aeedbcbd0b77b5a34c7f650
roddg86 <roddg@mail.ru>
2022-08-12 13:53:45 +0900

    Lambdas exercise

--- Lambdas/src/main/kotlin/Lambdas.kt
+++ Lambdas/src/main/kotlin/Lambdas.kt
@@ -1,28 +1,17 @@
+fun unless(condition: Boolean, code: () -> Unit) {
+    if (!condition) {
+        code()
+    }
+}
+
 fun main() {
-
-    /* функция преобразования температуры по Цельсию в температуру по Фаренгейту */
-    fun convert(x: Double, converter: (Double) -> Double): Double {
-        val result = converter(x)
-        println("$x преобразуется в $result")
-        return result
+    val options = arrayOf("Red", "Amber", "Green")
+    var crossWalk = options[(Math.random() * options.size).toInt()]
+    if (crossWalk == "Green") {
+        println("Walk!")
     }
-
-    /* вызов функции аргументом которой будет лямбда выражение */
-    convert(20.0) { c: Double -> c * 1.8 + 32 }
-
-    /* краткая запись */
-    val fahrenheit = convert(36.6) { it * 1.8 + 32 }
-
-    fun convertFive(converter: (Int) -> Double): Double {
-        val result = converter(5)
-        println("5 преобразуется в $result")
-        return result
+    unless(crossWalk == "Green") {
+        println("Stop!")
     }
-
-    /* краткая запись, при вызове функции мы не используем круглые скобки потому что единственный параметр это лямбда */
-    convertFive {
-        it * 1.8 + 32
-    }
-    convert(22.0) { it * 1.8 + 32 }
 
 }
