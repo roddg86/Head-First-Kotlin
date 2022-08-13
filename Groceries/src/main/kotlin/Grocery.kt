b0316521f1ad440cb3f4e1d9cbcea855c547cb9e
roddg86 <roddg@mail.ru>
2022-08-13 19:02:49 +0900

    sumOf

--- Groceries/src/main/kotlin/Grocery.kt
+++ Groceries/src/main/kotlin/Grocery.kt
@@ -7,7 +7,7 @@ data class Grocery(
 )
 
 fun main() {
-    val griceries = listOf(
+    val groceries = listOf(
         Grocery("Tomatoes", "Vegetable", "lb", 3.0, 3),
         Grocery("Mushrooms", "Vegetable", "lb", 4.0, 1),
         Grocery("Bagels", "Bakery", "Pack", 1.5, 2),
@@ -23,10 +23,19 @@ fun main() {
     println(minInt)
 
     /* Найти в коллекции groceries элемент с наибольшим значением unitPrice */
-    val highestUnitPrice = griceries.maxBy { it.unitPrice }
+    val highestUnitPrice = groceries.maxBy { it.unitPrice }
     println(highestUnitPrice)
 
     /* Найти в коллекции groceries элемент с наименьшим значением quantity */
-    val lowestQuantity = griceries.minBy { it.quantity }
+    val lowestQuantity = groceries.minBy { it.quantity }
     println(lowestQuantity)
+
+    /* Найти сумму значений обьекта коллекции, возвращает сумму всех значений quantity в groceries */
+    val sumQuantity = groceries.sumOf { it.quantity }
+    println(sumQuantity)
+
+    /* Возвращает сумму всех значений unitPrice уноженных на quantity */
+    val totalPrice = groceries.sumOf { it.quantity * it.unitPrice }
+    println(totalPrice)
+
 }
