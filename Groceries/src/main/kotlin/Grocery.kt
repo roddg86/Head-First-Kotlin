6c75d234e742fc20ccc1d2b15ef76fbee531ee35
roddg86 <roddg@mail.ru>
2022-08-13 21:15:59 +0900

    forEach

--- Groceries/src/main/kotlin/Grocery.kt
+++ Groceries/src/main/kotlin/Grocery.kt
@@ -17,46 +17,14 @@ fun main() {
         Grocery("Ice cream", "Frozen", "Pack", 3.0, 2)
     )
 
-    /* Найдем наименьшее и наибольшее значение в коллекции обьектов */
-    val ints = listOf(1, 2, 3, 4)
-    val maxInt = ints.max() //maxInt = 4
-    val minInt = ints.min() //maxInt = 1
-    println("maxInt: $maxInt")
-    println("minInt: $minInt")
-
-    /* Найти в коллекции groceries элемент с наибольшим значением unitPrice */
-    val highestUnitPrice = groceries.maxBy { it.unitPrice * 5 }
-    println("highestUnitPrice: $highestUnitPrice")
-
-    /* Найти в коллекции groceries элемент с наименьшим значением quantity */
-    val lowestQuantity = groceries.minBy { it.quantity }
-    println("lowestQuantity: $lowestQuantity")
-
-    /* Найти сумму значений обьекта коллекции, возвращает сумму всех значений quantity в groceries */
-    val sumQuantity = groceries.sumOf { it.quantity }
-    println("sumQuantity: $sumQuantity")
-
-    /* Возвращает сумму всех значений unitPrice уноженных на quantity */
-    val totalPrice = groceries.sumOf { it.quantity * it.unitPrice }
-    println("totalPrice: $totalPrice")
-
     /* Функция фильтр, отбирает коллекцию по критерию переданому в лямбда выражении */
     val vegetables = groceries.filter { it.category == "Vegetable" }
     println("vegetables: $vegetables")
-
-    /* возвращает список List с элементами Grocery, у которых значение unitPrice больше 3.0 */
-    val unitPriceOvar3 = groceries.filter { it.unitPrice > 3.0 }
-    println("unitPriceOvar3: $unitPriceOvar3")
 
     /* функция filterNot возвращает список всех элементов Grocery,
     у которых значение category отлично от «Frozen» */
     val notFrozen = groceries.filterNot { it.category == "Frozen" }
     println("notFrozen: $notFrozen")
-
-    /* с помощью функции map требуется создать новый список
-    List<Int> с теми же элементами, умноженными на 2 */
-    val doubleInts = ints.map { it * 2 }
-    println("doubleInts: $doubleInts")
 
     /* Создаем новый список List и заполняем его значениями свойства name каждого элемента Grocery в groceries */
     val groceryNames = groceries.map { it.name }
@@ -69,31 +37,16 @@ fun main() {
     /* обьединение в цепочку функций высшего порядка */
     val newPrices = groceries.filter { it.unitPrice > 3.0 }
         .map { it.unitPrice * 2 }
-    println(newPrices)
+    println("newPrices: $newPrices")
 
-    /* перебрать все элементы списка groceries и вывести
-    значение name каждого элемента */
-    /* for */
-    for (item in groceries) {
-        println(item.name)
-    }
-
-    /* forEach */
+    println("Grocery names: ")
     groceries.forEach { println(it.name) }
 
-    /* обьединим цепочки вызовов с помощью forEach */
+    println("Groceries with unitPrice > 3.0: ")
     groceries.filter { it.unitPrice > 3.0 }
         .forEach { println(it.name) }
 
-    /* Переменную itemNames можно обновлять внутри тела цикла for. */
     var itemNames = ""
-    for (item in groceries) {
-        itemNames += "${item.name} "
-    }
+    groceries.forEach({ itemNames += "${it.name}" })
     println("itemNames: $itemNames")
-
-    /* Переменную itemNames также можно обновлять в теле лямбда-выражения, передаваемого forEach. */
-    var itemNames2 = ""
-    groceries.forEach{ itemNames2 += "${it.name} " } // лямбда-выражение может обращаться к своему замыканию
-    println("itemNames2: $itemNames2")
 }
