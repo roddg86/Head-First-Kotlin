c5f9a8848f2a3b836f5b4580a4a13d9796086d18
roddg86 <roddg@mail.ru>
2022-08-13 19:31:13 +0900

    Exercises with functions in collections

--- Lambdas/src/main/kotlin/Main.kt
+++ Lambdas/src/main/kotlin/Main.kt
@@ -1,32 +1,13 @@
-fun search(
-    list: List<Grocery>,
-    criteria: (g: Grocery) -> Boolean
-) {
-    for (l in list) {
-        if (criteria(l)) {
-                println(l.name)
-            }
+fun myFun5(
+    x: (Int) -> Int = {
+        println(it)
+        it + 7
     }
+) {
+    x(4)
 }
 
-data class Grocery(
-    val name: String, val category: String,
-    val unit: String, val unitPrice: Double
-)
 
-fun main(args: Array<String>) {
-    /*    Функция main использует функцию search.*/
-    val groceries = listOf(
-        Grocery("Tomatoes", "Vegetable", "lb", 3.0),
-        Grocery("Mushrooms", "Vegetable", "lb", 4.0),
-        Grocery("Bagels", "Bakery", "Pack", 1.5),
-        Grocery("Olive oil", "Pantry", "Bottle", 6.0),
-        Grocery("Ice cream", "Frozen", "Pack", 3.0)
-    )
-    println("Expensive ingredients:")
-    search(groceries) { i: Grocery -> i.unitPrice > 5.0 }
-    println("All vegetables:")
-    search(groceries) { i: Grocery -> i.category == "Vegetable" }
-    println("All packs:")
-    search(groceries) { i: Grocery -> i.unit == "Pack" }
+fun main() {
+
 }
